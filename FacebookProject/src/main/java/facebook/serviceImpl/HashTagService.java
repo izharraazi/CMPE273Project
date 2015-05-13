2d1a3421f8a6cf8defc1db4ac6e6409712606675
Dhwani Shah <dhwani.shah0912@gmail.com>
2015-05-13 15:39:13 -0700

    Hash Tags Service

--- FacebookProject/src/main/java/facebook/serviceImpl/HashTagService.java
+++ FacebookProject/src/main/java/facebook/serviceImpl/HashTagService.java
@@ -1,28 +1,14 @@
 package facebook.serviceImpl;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-
-
-
 import com.restfb.Connection;
 import com.restfb.FacebookClient;
 import com.restfb.Parameter;
 import com.restfb.types.Post;
-import com.restfb.types.Post.Comments;
-import com.restfb.types.Post.Likes;
-
 import facebook.model.UserHashTag;
-import facebook.model.UserHomeFeed;
-import facebook.repository.HashTagRepo;
 import facebook.service.HashTag;
 /*
  * This service is to fetch all the Trending Hashtags that has been posted on UserHome 
@@ -32,45 +18,46 @@ import facebook.service.HashTag;
 public class HashTagService implements HashTag {
 	
 	
-	 
+	 ArrayList<String> hashtag = new ArrayList<String>();
+	 ArrayList<UserHashTag> hashtagList = new ArrayList<UserHashTag>();
 	@Override
-	public List<UserHashTag> findTrendingHashTags(FacebookClient fbClient,
+	public List<String> findTrendingHashTags(FacebookClient fbClient,
 			String userId) {
-		// TODO Auto-generated method stub
 		
-		Connection<Post> UserPost = fbClient.fetchConnection("me/home", Post.class, Parameter.with("fields","id,message,from"));
+		hashtag = new ArrayList<String>();
+		Connection<Post> UserPost = fbClient.fetchConnection("me/home", Post.class, Parameter.with("fields","id,message,from"),Parameter.with("limit", 10));
 		Pattern MY_PATTERN = Pattern.compile("#(\\w+)");			//RegEx Pattern to search  all the HashTags 
-		ArrayList<String> hashtag = new ArrayList<String>();
-		ArrayList<UserHashTag> hashtagList = new ArrayList<UserHashTag>();
+		
 		Matcher mat ;
 		System.out.println("HashTagService.findTrendingHashTags() I am inside Hashtags");
-		try{
+	try{
+				
 			for (Post posts: UserPost.getData()){
-				if(posts.getMessage()!=null){
-					hashtag.clear();
-					UserHashTag hash_tags   = new UserHashTag();
-					mat = MY_PATTERN.matcher(posts.getMessage());   //PatternMatching to segregate all the HashTags from the Posts        
-					while (mat.find()) {
-						System.out.println("HashTagService.findTrendingHashTags()------------------"+mat.group(1));
-						hashtag.add(mat.group(1));
-					}
-					if(hashtag.size()>0 && !hashtag.isEmpty()){
-						hash_tags.setHashTag(hashtag);
-						hash_tags.setId(posts.getId().toString());
-						hash_tags.setCreatedBy(posts.getFrom().toString());
-						hashtagList.add(hash_tags);
-					}
-				}
-			}					 
-
-		}
-		catch(Exception e){
+					if(posts.getMessage()!=null){
+						//hashtag.clear();
+					    	UserHashTag hash_tags   = new UserHashTag();
+						    mat = MY_PATTERN.matcher(posts.getMessage());   //PatternMatching to segregate all the HashTags from the Posts        
+							while (mat.find()) {
+							 	System.out.println("HashTagService.findTrendingHashTags()------------------"+mat.group(1));
+							 	//hashtag.add("testhashtag");
+							 	hashtag.add(mat.group(1));
+							}
+							if(hashtag.size()>0 && !hashtag.isEmpty()){
+										//hashtag.add("testhashtag");
+							 	  		hash_tags.setHashTag(hashtag);
+								 	  	hash_tags.setId(posts.getId().toString());
+								 	  	hash_tags.setCreatedBy(posts.getFrom().toString());
+								 	  	hashtagList.add(hash_tags);
+							 	  	}
+						 	  }
+			            }
+			    	
+	   	 }
+			catch(Exception e){
 		e.printStackTrace();
 	}
-	return hashtagList;
+	return hashtag;
 		
 	}
 	
 }
-
-	
