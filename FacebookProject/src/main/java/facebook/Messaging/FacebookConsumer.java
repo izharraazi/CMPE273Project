ea7dbea438b26d72dd7051b287b1336eeb65b562
izharraazi <izharraazi@gmail.com>
2015-05-18 12:30:01 -0700

    Update FacebookConsumer.java
    skipped to commit from Local Repo

--- FacebookProject/src/main/java/facebook/Messaging/FacebookConsumer.java
+++ FacebookProject/src/main/java/facebook/Messaging/FacebookConsumer.java
@@ -1,15 +1,18 @@
 package facebook.Messaging;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
 import kafka.consumer.Consumer;
 import kafka.consumer.ConsumerConfig;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.util.StringUtils;
 
-import java.util.*;
+import org.springframework.util.StringUtils;
 
 /*
  * Created by IRaazi 
@@ -17,6 +20,41 @@ import java.util.*;
 */
 public class FacebookConsumer {
 
-   
+    private final ConsumerConnector consumer;
+    private final String topic;
+    FacebookEmailService emailService = new FacebookEmailService();
 
+    public FacebookConsumer(String zookeeper, String groupId, String topic) {
+        Properties props = new Properties();
+        props.put("zookeeper.connect", zookeeper);
+        props.put("group.id", groupId);
+        props.put("zookeeper.session.timeout.ms", "700");
+        props.put("zookeeper.sync.time.ms", "250");
+        props.put("auto.commit.interval.ms", "1000");
+
+        consumer = Consumer.createJavaConsumerConnector(new ConsumerConfig(props));
+        this.topic = topic;
+    }
+
+    public void getMessage(String email) {
+        Map<String, Integer> topicCount = new HashMap<>();
+        boolean result;
+        topicCount.put(topic, 1);
+        
+        Map<String, List<KafkaStream<byte[], byte[]>>> consumerStreams = consumer.createMessageStreams(topicCount);
+        List<KafkaStream<byte[], byte[]>> streams = consumerStreams.get(topic);
+        for (final KafkaStream stream : streams) {
+            ConsumerIterator<byte[], byte[]> it = stream.iterator();
+            while (it.hasNext()) {
+                String msg = new String(it.next().message());
+                System.out.println("#####################################################FacebookConsumer.consume()########################"+msg);
+                 result = emailService.sendMail(msg,email);
+            }
+        }
+        if (consumer != null) {
+            consumer.shutdown();
+        }
+    }
+
+    
 }
